fa0313b590b23bbc20d728de676a9dd6d28c0b11
Orlando <el31hippie@hotmail.com>
2013-03-15 17:51:36 +0100

    Inicializacion16

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_4_3_16
+{
+    class Persona16
+    {
+        public Persona16(int altura, int peso, string colorOjos, string colorPelo)
+        {
+            this.altura16 = altura;
+            this.colorOjos16 = colorOjos;
+            this.colorPelo16 = colorPelo;
+            this.peso16 = peso;
+        }
+
+        public int peso16;
+        public int altura16;
+        public string colorOjos16;
+        public string colorPelo16;
+
+        public void PonerLentillas16(string colorLentillas)
+        {
+
+        }
+
+        public void Engordar16(int cantidad)
+        {
+
+        }
+
+        public void Adelgazar16(double cantidad)
+        {
+
+        }
+    }
+}
